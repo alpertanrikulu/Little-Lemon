ec043dad54125019c9c8c338122c594599c6b0dd
Alper Tanrıkulu <tanrikulualper77100@gmail.com>
2025-09-29 17:25:59 +0300

    add to basket property added

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,34 +11,34 @@ interface Props {
 
 const Card: React.FC<Props> = ({ image, title, price, description }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [newItemCount, setNewItemCount] = useState(0);
   return (
     <div className="w-[250px] bg-lemongray rounded-[16px] h-[500px] flex flex-col justify-between mt-[16px] md:mt-[0px]">
       <img
         className="rounded-t-[16px]"
         src={image}
-        alt="greek salad"
+        alt={title}
         width={250}
         height={300}
       />
       <div className="p-[16px] flex flex-col justify-between gap-[24px] h-full">
         {isOpen ? (
           <div className="flex flex-col items-center justify-center text-center h-full gap-4">
-            
-              <div className="w-16 h-16 rounded-full bg-[#48742C] flex items-center justify-center mb-6">
-                <svg
-                  className="w-8 h-8 text-white"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M5 13l4 4L19 7"
-                  />
-                </svg>
-              </div>
+            <div className="w-16 h-16 rounded-full bg-[#48742C] flex items-center justify-center mb-6">
+              <svg
+                className="w-8 h-8 text-white"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+            </div>
             <h3 className="font-karla font-bold text-[18px] text-green-700">
               Added to Basket
             </h3>
@@ -63,12 +63,15 @@ const Card: React.FC<Props> = ({ image, title, price, description }) => {
             <p>{description}</p>
             <div className="flex justify-between items-center">
               <button
-                onClick={() => setIsOpen(true)}
+                onClick={() => {
+                  setIsOpen(true);
+                  setNewItemCount((count) => count + 1);
+                }}
                 className="font-karla font-medium text-[16px] bg-lemonyellow text-black px-[16px] py-[8px] rounded-[16px]"
               >
                 Add to Basket
               </button>
-              <Package />
+              {newItemCount ? <p className="font-karla font-medium text-[18px]">{newItemCount}</p> : <Package />}
             </div>
           </>
         )}
